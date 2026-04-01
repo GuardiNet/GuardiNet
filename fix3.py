be2e0069321c17c4b8a13906f1c97472f6d5832d
Swy'z <mpoissonnier@guardiaschool.fr>
2026-04-01 13:36:57 +0200

    Fix last code

--- fix3.py
+++
@@ -1,5 +0,0 @@
-with open("app/routes/main.py", "r", encoding="utf-8") as f:
-    text = f.read()
-text = text.replace("            | (Announcement.class_id.is_(None))", "            (Announcement.class_id.is_(None))")
-with open("app/routes/main.py", "w", encoding="utf-8") as f:
-    f.write(text)
--- fix_w291.py
+++
@@ -1,5 +0,0 @@
-with open("app/routes/main.py", "r", encoding="utf-8") as f:
-    text = f.read()
-text = text.replace(" (Announcement.class_id.in_(class_ids)) \n", " (Announcement.class_id.in_(class_ids))\n")
-with open("app/routes/main.py", "w", encoding="utf-8") as f:
-    f.write(text)
